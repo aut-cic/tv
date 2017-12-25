bebe33fff21f784e71d4926fb4b49aa114c4e979
Pooya Parsa <pooya@pi0.ir>
2017-12-25 19:27:14 +0330

    remove extra code

--- plugins/app.js
+++ plugins/app.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import VueVideoPlayer from 'vue-video-player'
-// import { directive as onClickaway } from 'vue-clickaway';
 
 import 'video.js/dist/video-js.css'
 import 'video.js/dist/lang/fa'
 
 import 'videojs-contrib-hls.js'
-
-// Vue.directive('onClickaway', onClickaway)
 
 Vue.use(VueVideoPlayer)
 
