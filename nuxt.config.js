e0115d7f1fba69e7b3ba4a21b33e7a74525ba780
Parham Alvani <parham.alvani@gmail.com>
2020-12-28 01:47:31 +0330

    feat: Improve Nuxt Configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,36 @@
 module.exports = {
   ssr: false,
+  dev: false,
 
   build: {
-    extractCSS: true
+    extractCSS: true,
   },
 
   manifest: {
-    name: 'تلویزیون اینترنتی امیرکبیر',
-    short_name: 'تلویزیون امیرکبیر',
-    dir: 'rtl',
-    theme_color: '#232323'
+    name: "تلویزیون اینترنتی امیرکبیر",
+    short_name: "تلویزیون امیرکبیر",
+    dir: "rtl",
+    theme_color: "#232323",
   },
 
   loading: false,
 
   loadingIndicator: {
     name: false,
-    color: 'grey',
-    background: '#131313'
+    color: "grey",
+    background: "#131313",
   },
 
-  modules: [
-    ['@nuxtjs/pwa', { workbox: false }],
-    '@nuxtjs/axios',
-    '@nuxtjs/google-analytics'
-  ],
+  modules: [["@nuxtjs/pwa", { workbox: false }], "@nuxtjs/axios"],
 
-  'google-analytics': {
-    id: 'UA-108642664-1',
-    autoTracking: {
-      exception: true
-    }
-  },
-
-  plugins: [
-    '~/plugins/app'
-  ],
+  plugins: ["~/plugins/app"],
 
   serverMiddleware: [
     {
-      path: '/api/',
-      handler: '~/api'
-    }
+      path: "/api/",
+      handler: "~/api",
+    },
   ],
 
-  css: [
-    '~/assets/css/app.css'
-  ],
-}
+  css: ["~/assets/css/app.css"],
+};
