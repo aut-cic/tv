4c645d11017ebe37902d73e59f2cc05bc7ca2056
Parham Alvani <parham.alvani@gmail.com>
2020-12-28 10:40:06 +0330

    fix: Correct Paths

--- store/index.js
+++ store/index.js
@@ -1,34 +1,34 @@
-import Vue from 'vue'
+import Vue from "vue";
 
 export default {
-    state() {
-        return {
-            channels: {}, // slug => channel
-            info: {},     // slug => channel info
-        }
+  state() {
+    return {
+      channels: {}, // slug => channel
+      info: {}, // slug => channel info
+    };
+  },
+  mutations: {
+    SET_CHANNELS(state, channels) {
+      state.channels = {};
+      channels.forEach((channel) => {
+        state.channels[channel.slug] = channel;
+      });
     },
-    mutations: {
-        SET_CHANNELS(state, channels) {
-            state.channels = {}
-            channels.forEach(channel => {
-                state.channels[channel.slug] = channel
-            })
-        },
-        SET_CHANNEL(state, { slug, info }) {
-            if (!state.channels[slug]) {
-                return
-            }
-            Vue.set(state.info, slug, info)
-        }
+    SET_CHANNEL(state, { slug, info }) {
+      if (!state.channels[slug]) {
+        return;
+      }
+      Vue.set(state.info, slug, info);
     },
-    actions: {
-        async FETCH_CHANNELS({ commit }) {
-            const { channels } = await this.$axios.$get('http://tv2.aut.ac.ir/api/channels')
-            commit('SET_CHANNELS', channels)
-        },
-        async FETCH_CHANNEL({ commit }, slug) {
-            const { info } = await this.$axios.$get('http://tv2.aut.ac.ir/api/channels/' + slug)
-            commit('SET_CHANNEL', { slug, info })
-        }
-    }
-}
+  },
+  actions: {
+    async FETCH_CHANNELS({ commit }) {
+      const { channels } = await this.$axios.$get("/api/channels");
+      commit("SET_CHANNELS", channels);
+    },
+    async FETCH_CHANNEL({ commit }, slug) {
+      const { info } = await this.$axios.$get("/api/channels/" + slug);
+      commit("SET_CHANNEL", { slug, info });
+    },
+  },
+};
